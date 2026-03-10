f94f07eb5a636f6b5fbbbd58cf393d41384f4561
2hyeon-jeong <2hyeon.dev@gmail.com>
2026-03-10 23:43:15 +0900

    feat/graduation-requirement-controller

--- src/main/java/com/dongguk/graduation_be/requirement/repository/GraduationRequirementRepository.java
+++ src/main/java/com/dongguk/graduation_be/requirement/repository/GraduationRequirementRepository.java
@@ -1,7 +1,23 @@
 package com.dongguk.graduation_be.requirement.repository;
 
+import com.dongguk.graduation_be.requirement.entity.Curriculum;
 import com.dongguk.graduation_be.requirement.entity.GraduationRequirement;
+import com.dongguk.graduation_be.requirement.entity.MajorType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GraduationRequirementRepository extends JpaRepository<GraduationRequirement, Long> {
+    boolean existsByEntranceYearAndDepartmentIdAndCurriculumAndMajorType(
+            Integer entranceYear,
+            Long departmentId,
+            Curriculum curriculum,
+            MajorType majorType
+    );
+
+    boolean existsByEntranceYearAndDepartmentIdAndCurriculumAndMajorTypeAndIdNot(
+            Integer entranceYear,
+            Long departmentId,
+            Curriculum curriculum,
+            MajorType majorType,
+            Long id
+    );
 }
